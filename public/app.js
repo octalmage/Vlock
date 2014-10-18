e2ea44e9dfdf52bc57712ce99e639804f997e8a2
Jason Stallings <jason@stallin.gs>
2014-10-18 11:14:50 -0500

    Initial commit, basic app working.

---
+++ public/app.js
@@ -0,0 +1,46 @@
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var moment = require('moment');
+var time;
+var t;
+
+if (process.platform === "darwin")
+{
+	var mb = new gui.Menu(
+	{
+		type: "menubar"
+	});
+	mb.createMacBuiltin("Marknote");
+	win.menu = mb;
+}
+
+$(document).on("ready",function()
+{
+
+	win.showDevTools();
+
+	$("#startButton").on("click", function()
+	{
+		today=moment().format("YYYY-M-D");  
+		time=moment(today + " " + $("#time").val());
+
+		checkTime();
+
+		t=setInterval(function()
+		{
+			checkTime();
+		},10000);
+	})
+    
+ })  
+
+function checkTime()
+{
+	console.log(moment().format("HHmm") + " = " + time.format("HHmm"));
+	if (moment().format("HHmm")==time.format("HHmm"))
+	{
+		clearTimeout(t);
+		win.enterFullscreen();
+		loadvideoplayer("EfDfdyBldz0");
+	}
+}
