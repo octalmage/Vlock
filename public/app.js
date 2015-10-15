7cd2cf18c60807d5ea43eb978f0d3d2f9f9adcaf
Jason Stallings <jason@stallin.gs>
2015-10-15 02:18:51 -0500

    New code to make YouTube video responsive.

--- public/app.js
+++ public/app.js
@@ -45,6 +45,19 @@ function checkTime()
 		$("#controls").hide();
 		win.show();
 		win.enterFullscreen();
+		
+		win.on("enter-fullscreen", function()
+		{
+			loadvideoplayer($("#video").val());
+			
+			//Hack to fix YouTube video size.
+			setTimeout(function()
+			{
+				win.resizeBy(-1, -1);
+				win.resizeBy(1, 1);
+			}, 2000);
+	
+		});
 
 		loadvideoplayer($("#video").val());
 	}
